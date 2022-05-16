e07808f931f675de8d1e6b71efdff6f990e96548
dominikhubner <dominikhubner@pm.me>
2022-05-16 10:59:09 +0200

    User eingabe prototyp

--- Kalender/Program.cs
+++ Kalender/Program.cs
@@ -6,7 +6,11 @@ namespace Kalender
     {
         private static void Main(string[] args)
         {
-            Kalenderblatt kalenderblatt = new(6, 2022);
+            Console.Write("Monat: ");
+            int monat = int.Parse(Console.ReadLine());
+            Console.Write("Jahr: ");
+            int jahr = int.Parse(Console.ReadLine());
+            Kalenderblatt kalenderblatt = new(monat, jahr);
             kalenderblatt.PrintKalender();
         }
     }
