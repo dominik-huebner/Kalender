103f2774ccccd9215c1a99f1fb82c6f6227fb76e
dominikhubner <dominikhubner@pm.me>
2022-05-18 13:53:21 +0200

    User Input

--- Kalender/Program.cs
+++ Kalender/Program.cs
@@ -4,14 +4,58 @@ namespace Kalender
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            Console.Write("Monat: ");
-            int monat = int.Parse(Console.ReadLine());
-            Console.Write("Jahr: ");
-            int jahr = int.Parse(Console.ReadLine());
-            Kalenderblatt kalenderblatt = new(monat, jahr);
-            kalenderblatt.PrintKalender();
+            int monat = 0;
+            int jahr = 0;
+
+            while (true)
+            {
+                bool monatSuccess = false;
+                bool jahrSuccess = false;
+
+                while (!monatSuccess)
+                {
+                    Console.Write("Monat: ");
+                    string inputMonat = Console.ReadLine();
+                    monatSuccess = int.TryParse(inputMonat, out monat);
+
+                    if (!monatSuccess || monat > 12 || monat < 1)
+                    {
+                        if (string.Equals(inputMonat.ToLower(), "exit"))
+                        {
+                            Environment.Exit(0);
+                        }
+
+                        monatSuccess = false;
+                        Console.WriteLine("Gib eine Zahl zwischen 1 (Januar) und 12 (Dezember) ein \nGib exit ein um das Programm zu beenden");
+                    }
+                }
+
+                while (!jahrSuccess)
+                {
+                    Console.Write("Jahr: ");
+                    string inputJahr = Console.ReadLine();
+                    jahrSuccess = int.TryParse(inputJahr, out jahr);
+
+                    if (!jahrSuccess || jahr > 3000 || jahr < 1582)
+                    {
+                        if (string.Equals(inputJahr.ToLower(), "exit"))
+                        {
+                            Environment.Exit(0);
+                        }
+
+                        jahrSuccess = false;
+                        Console.WriteLine("Gib ein Jahr zwischen 1582 und 3000 ein \nGib exit ein um das Programm zu beenden");
+                    }
+                }
+
+                if (monatSuccess && jahrSuccess)
+                {
+                    Kalenderblatt kalenderblatt = new(monat, jahr);
+                    kalenderblatt.PrintKalender();
+                }
+            }
         }
     }
 }
